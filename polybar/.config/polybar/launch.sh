a9b6cb4f35741cf2dfd0571ba1f5273d37f9f7a4
RaidLancea <126603482+RaidLancea@users.noreply.github.com>
2024-10-13 01:30:55 +0100

    commit initial dotfiles

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# from arch wiki
+if type "xrandr"; then
+	for m in $(xrandr --query | grep "connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload mybar &
+	done
+else
+	polybar --reload mybar &
+fi
